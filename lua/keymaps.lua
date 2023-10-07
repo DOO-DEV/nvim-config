a1771fdea71f9f6cafa5d86f90a07e0f4b57391e
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-10-07 23:17:26 +0330

    add more keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,20 @@ map('', '<Space>', '<Nop>', o) -- unbind space
 vim.g.mapleader = ' ' -- map leader key to space
 
 
+map('n', 'U', '<C-R>', o)
+map('n', '<C-s>', '<cmd>w<CR>', o)
+map('n', 'x', '"_x', o) -- delete without yanking
+map('n', 'yw', 'yiw', o) -- yank a word from anywhere
+map('n', 'dw', '"_diw', o) -- delete a word from anywhere without yanking
+map('n', 'cw', '"_ciw', o) -- change a word from anywhere without yanking
+map('n', 'cc', '"_cc', o) -- change line without yanking
+map('v', 'c', '"_c', o) -- change selection without yanking
+map('v', 'p', '"_dP', o) -- override selected word without yanking it
+map('x', 'p', '"_dP', o)
+map('n', 'ss', '<cmd>split<CR>', o)
+map('n', 'sv', '<cmd>vsplit<CR>', o)
+map('n', '<leader>r', '<cmd>%s/', { noremap = true })
+
 -- lsp keymaps
 map('n', '<leader><leader>', '<cmd>lua vim.lsp.buf.hover()<CR>', o)
 map('n', 'gr', '<cmd>lua vim.lsp.buf.rename()<CR>', o)
